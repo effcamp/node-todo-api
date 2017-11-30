9ac68a16594123ab42c592e721908a14bb9af104
effcamp <fcf.camposx@gmail.com>
2017-11-29 22:01:34 -0200

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,39 @@ describe('GET /todos/:id', () => {
       .end(done)
   })
 })
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', done => {
+    const hexID = todos[1]._id.toHexString()
+    request(app)
+      .delete(`/todos/${hexID}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexID)
+        // expect(res.body.todos.length).toBe(1)
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err)
+        }
+        Todo.findById(hexID)
+          .then(todo => {
+            expect(todo).toNotExist()
+            done()
+          })
+          .catch(e => done(e))
+      })
+  })
+  it('should return 404 if todo not found', done => {
+    const id = new ObjectID().toHexString() //real ID but not in test-todos
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(404)
+      .end(done)
+  })
+  it('should return 404 if object id is invalid', done => {
+    request(app)
+      .delete(`/todos/123abc`)
+      .expect(404)
+      .end(done)
+  })
+})
