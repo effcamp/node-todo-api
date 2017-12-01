38fea640d681c9a43a750910a3edfb0f2c0bc83e
effcamp <fcf.camposx@gmail.com>
2017-12-01 17:02:45 -0200

    Add POST /users route and token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,14 +1,67 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const jwt = require('jsonwebtoken')
+const _ = require('lodash')
 
-const userModel = {
+const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: value => {
+        return validator.isEmail(value)
+      },
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  },
+  tokens: [
+    {
+      access: {
+        type: String,
+        required: true
+      },
+      token: {
+        type: String,
+        required: true
+      }
+    }
+  ]
+})
+
+UserSchema.methods.toJSON = function() {
+  let user = this
+  const userObject = user.toObject()
+
+  return _.pick(userObject, ['_id', 'email'])
 }
 
-const User = mongoose.model('User', userModel)
+UserSchema.methods.generateAuthToken = function() {
+  let user = this
+  const access = 'auth'
+  token = jwt
+    .sign(
+      {
+        _id: user._id.toHexString(),
+        access
+      },
+      'abc123'
+    )
+    .toString()
+  user.tokens.push({ access, token })
+
+  return user.save().then(() => {
+    return token
+  })
+}
+
+const User = mongoose.model('User', UserSchema)
 
 module.exports = { User }
--- server/server.js
+++ server/server.js
@@ -93,8 +93,35 @@ app.patch('/todos/:id', (req, res) => {
     .catch(e => res.status(400).send())
 })
 
+app.post('/users', (req, res) => {
+  const body = _.pick(req.body, ['email', 'password'])
+  const user = new User(body)
+  user
+    .save()
+    .then(() => {
+      return user.generateAuthToken()
+    })
+    .then(token => {
+      res.header('x-auth', token).send(user)
+    })
+    .catch(e => res.status(400).send(e))
+})
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`)
 })
 
 module.exports = { app }
+
+// app.post('/todos', (req, res) => {
+//   const todo = new Todo({
+//     text: req.body.text
+//   })
+
+//   todo
+//     .save()
+//     .then(doc => {
+//       res.send(doc)
+//     })
+//     .catch(e => res.status(400).send(e))
+// })
