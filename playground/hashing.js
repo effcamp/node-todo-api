2052d0c35e369283a0435b59142e19a6fb414180
effcamp <fcf.camposx@gmail.com>
2017-12-03 21:38:47 -0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,29 @@
 const { SHA256 } = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-const data = {
-  id: 10
-}
-const token = jwt.sign(data, '123abc')
-console.log(token)
+let password = '123abc!'
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash)
+//   })
+// })
 
-const decoded = jwt.verify(token, '123abc')
-console.log(`Decoded`, decoded)
+const hashedPassword =
+  '$2a$10$wGKcxIW.ep7LsE419/QIFOS9MRhz0fNfkVHTnoU/M/S3gPtdfoi56'
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res)
+})
+
+// const data = {
+//   id: 10
+// }
+// const token = jwt.sign(data, '123abc')
+// console.log(token)
+
+// const decoded = jwt.verify(token, '123abc')
+// console.log(`Decoded`, decoded)
 
 // const message = 'I am user number 3'
 
--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const jwt = require('jsonwebtoken')
 const _ = require('lodash')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   email: {
@@ -63,7 +64,7 @@ UserSchema.methods.generateAuthToken = function() {
 }
 
 UserSchema.statics.findByToken = function(token) {
-  const User = this
+  let User = this
   let decoded
 
   try {
@@ -78,6 +79,21 @@ UserSchema.statics.findByToken = function(token) {
     'tokens.access': 'auth'
   })
 }
+
+UserSchema.pre('save', function(next) {
+  let user = this
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash
+        next()
+      })
+    })
+  } else {
+    next()
+  }
+})
 
 const User = mongoose.model('User', UserSchema)
 
